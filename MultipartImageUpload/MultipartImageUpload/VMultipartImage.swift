55c889e451cbfbec205ab2173756a3ca1416c68d
vishal kalola <26406461+vishalkalola1@users.noreply.github.com>
2018-06-22 11:42:04 +0530

    Add files via upload

--- MultipartImageUpload/MultipartImageUpload/VMultipartImage.swift
+++ MultipartImageUpload/MultipartImageUpload/VMultipartImage.swift
@@ -8,43 +8,45 @@
 
 import UIKit
 
-enum Method : String { //--- add Mehtod depends on your requirement
+public enum Method : String { //--- add Mehtod depends on your requirement
     case GET = "GET"
     case POST = "POST"
     
 }
 
-enum MimeType:String { //--- add MimeType depends on your requirement
+public enum MimeType:String { //--- add MimeType depends on your requirement
     case jpeg = "image/jpeg"
     case png = "image/png"
     case gif = "image/gif"
     
 }
 
-typealias Parameters = [String: String]
+public typealias Parameters = [String: String]
 
-class VMultipartImage: NSObject {
+open class VMultipartImage: NSObject {
     
-    var request : URLRequest!
+    public var request : URLRequest!
     
-    init(_ postUrl:String,paramters:Parameters, image:UIImage, imagekey:String, imageName:String) {
+    init(_ postUrl:String,paramters:Parameters, image:UIImage, imagekey:String, imageName:String, MimeType:MimeType? = .jpeg, Debug:Bool? = true) {
         super.init()
-        guard let mediaImage = MediaData(image, key: imagekey, imageName: imageName) else { return }
+        guard let mediaImage = MediaData(image, key: imagekey, imageName: imageName, mimeType:MimeType!) else { return }
         guard let url = URL(string: postUrl) else { return }
         request = URLRequest(url: url)
         request.httpMethod = Method.POST.rawValue
         let boundary = generateBoundary()
         request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        request.addValue("Client-ID f65203f7020dddc", forHTTPHeaderField: "Authorization") // ---- Testing code
+        if Debug! {
+            request.addValue("Client-ID f65203f7020dddc", forHTTPHeaderField: "Authorization") // ---- Testing code
+        }
         let dataBody = createDataBody(withParameters: paramters, media: [mediaImage], boundary: boundary)
         request?.httpBody = dataBody
     }
     
-    func generateBoundary() -> String {
+    fileprivate func generateBoundary() -> String {
         return "Boundary-\(NSUUID().uuidString)"
     }
     
-    func createDataBody(withParameters params: Parameters?, media: [MediaData]?, boundary: String) -> Data {
+    fileprivate func createDataBody(withParameters params: Parameters?, media: [MediaData]?, boundary: String) -> Data {
         
         let lineBreak = "\r\n"
         var body = Data()
@@ -73,15 +75,15 @@ class VMultipartImage: NSObject {
     }
 }
 
-struct MediaData {
+fileprivate struct MediaData {
     let key: String?
     let filename: String?
     let data: Data?
     let mimeType: String?
     
-    init?(_ image: UIImage, key: String, imageName: String) {
+    init?(_ image: UIImage, key: String, imageName: String, mimeType:MimeType) {
         self.key = key
-        self.mimeType = MimeType.jpeg.rawValue
+        self.mimeType = mimeType.rawValue
         self.filename = imageName
         guard let data = UIImageJPEGRepresentation(image, 0.7) else { return nil }
         self.data = data
